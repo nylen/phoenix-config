ddb043603634fc43c1ae528b7467d32651b3d5a8
James Nylen <jnylen@gmail.com>
2015-04-08 15:38:27 -0500

    Add tests for another grid

--- test/grid.js
+++ test/grid.js
@@ -3,37 +3,60 @@ var mocha = require('mocha'),
 
 var Grid = require('../Grid');
 
-describe('Grid', function() {
-    var retina = {
-            x : 12,
-            y : 8
-        },
-        grid;
-
-    beforeEach(function() {
-        var screens = {
-            thunderbolt : {
-                frameWithoutDockOrMenu : function() {
-                    return {
-                        x      : 0,
-                        y      : 0,
-                        width  : 12,
-                        height : 12
-                    };
-                }
-            },
-            retina : {
-                frameWithoutDockOrMenu : function() {
-                    return {
-                        x      : retina.x,
-                        y      : retina.y,
-                        width  : 12,
-                        height : 12
-                    };
-                }
+var retina = {
+        x : 12,
+        y : 8
+    },
+    screens = {
+        thunderbolt : {
+            frameWithoutDockOrMenu : function() {
+                return {
+                    x      : 0,
+                    y      : 0,
+                    width  : 12,
+                    height : 12
+                };
             }
-        };
+        },
+        retina : {
+            frameWithoutDockOrMenu : function() {
+                return {
+                    x      : retina.x,
+                    y      : retina.y,
+                    width  : 12,
+                    height : 12
+                };
+            }
+        }
+    },
+    grid;
 
+function checkOverlap(x1, w1, x2, w2, expected) {
+    Grid._overlap('x', 'w', { x:x1, w:w1 }, { x:x2, w:w2 }).must.eql(expected);
+}
+
+function checkSlot(x, y, dir, expected) {
+    it(dir + ' from x=' + x + ' y=' + y + ' == ' + expected, function() {
+        var slot = grid.slotInDirection({ x : x, y : y }, dir);
+        slot.name.must.eql(expected);
+    });
+}
+
+describe('Grid base functionality', function() {
+    it('calculates overlap between intervals', function() {
+        checkOverlap(1, 3,  6, 3,  -2);
+        checkOverlap(6, 3,  1, 3,  -2);
+
+        checkOverlap(2, 3,  1, 3,  +2);
+        checkOverlap(1, 3,  2, 3,  +2);
+
+        checkOverlap(1, 3,  2, 2,  +2);
+        checkOverlap(2, 2,  1, 3,  +2);
+    });
+});
+
+describe('Grid 1', function() {
+    beforeEach(function() {
         grid = new Grid(screens, 12, {
             thunderbolt : {
                 topLeft     : { x : [0,  6], y : [0,  6] },
@@ -46,28 +69,6 @@ describe('Grid', function() {
             }
         });
     });
-
-    it('calculates overlap between intervals', function() {
-        function checkOverlap(x1, w1, x2, w2, expected) {
-            Grid._overlap('x', 'w', { x:x1, w:w1 }, { x:x2, w:w2 }).must.eql(expected);
-        }
-
-        checkOverlap(1, 3,  6, 3,  -2);
-        checkOverlap(6, 3,  1, 3,  -2);
-
-        checkOverlap(2, 3,  1, 3,  +2);
-        checkOverlap(1, 3,  2, 3,  +2);
-
-        checkOverlap(1, 3,  2, 2,  +2);
-        checkOverlap(2, 2,  1, 3,  +2);
-    });
-
-    function checkSlot(x, y, dir, expected) {
-        it(dir + ' from x=' + x + ' y=' + y + ' == ' + expected, function() {
-            var slot = grid.slotInDirection({ x : x, y : y }, dir);
-            slot.name.must.eql(expected);
-        });
-    }
 
     checkSlot( 0,  0, 'left', 'topLeft');
     checkSlot( 4,  4, 'left', 'topLeft');
@@ -133,4 +134,25 @@ describe('Grid', function() {
 
     // maybe moving down from bottomLeft shouldn't go to the other screen?
     checkSlot(3, 9, 'down', 'full');
+});
+
+describe('Grid 2', function() {
+    beforeEach(function() {
+        grid = new Grid(screens, 12, {
+            thunderbolt : {
+                topLeft      : { x : [0,  6], y : [0,  7] },
+                topRight     : { x : [6, 12], y : [0,  7] },
+                bottomLeft   : { x : [0,  4], y : [7, 12] },
+                bottomMiddle : { x : [4,  8], y : [7, 12] },
+                bottomRight  : { x : [8, 12], y : [7, 12] }
+            },
+            retina : {
+                full : { x : [0, 12], y : [0, 12] }
+            }
+        });
+    });
+
+    checkSlot(0, 7, 'right', 'bottomMiddle');
+    checkSlot(4, 7, 'right', 'bottomRight');
+    checkSlot(8, 7, 'right', 'full');
 });
